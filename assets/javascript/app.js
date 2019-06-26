e41ce8000d5d2efa3d70e190aaf364f616cda5af
skish1987 <seankish1@gmail.com>
2019-06-26 11:26:09 -0700

    youtube js added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,7 @@ var musicalsArray=["Enchanted","La La Land", "A Star is Born", "Mamma Mia", "Les
 var indieArray=["Lady Bird", "The Big Sick", "Call me by Your Name", "Boyhood", "Tangerine", "Winter's Bone", "Manchester by the Sea", "Blue Jay", "Krisha", "Green Room"];
 var thrillerArray=["Annabelle", "Child's Play", "Ma", "Get Out", "Don't Breathe", "Seven", "The Silence of the Lambs", "Shutter Island", "Inception", "Zodiac" ];
 
-
+// Omdb api call
 
 $(".searchButton").on("click", function(e){
   e.preventDefault();
@@ -52,3 +52,41 @@ $(".searchButton").on("click", function(e){
       $(".movie-poster").append(poster);
   });
 });
+
+// start youtube api call
+$(".searchButton").on("click", function (e) {
+  e.preventDefault();
+  console.log("clickedfff")
+  var request = gapi.client.youtube.search.list({
+      kind: "youtube#searchResult",
+      part: "snippet",
+      type: "video",
+      q: $(".search-input").val().replace(/%20/g, "+"),
+      maxResults: 3,
+      order: "viewCount",
+  })
+  // execute request
+  request.execute(function (response) {
+      var str = response.result;
+      console.log(str)
+
+      $.each(response.result.items, function (index, item) {
+          $(".movie-trailer-video").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + item.id.videoId + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+      });
+  });
+  return false;
+});
+
+function init() {
+  console.log(gapi.client);
+
+  console.log('Search Started');
+  var apiKey = 'AIzaSyBi1ync9dV5CDPTmJmW135UexiYcMjmQWY';
+  var q = $('.search-input').val() + 
+
+  gapi.client.setApiKey(apiKey);
+  gapi.client.load('youtube', 'v3', function () {
+      isLoad = true;
+  });
+  console.log('Search Request');
+}
